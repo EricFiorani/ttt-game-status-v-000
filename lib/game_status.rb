b02a1e49f711e0955628ab56c003994b893bf589
Eric Fiorani <efiorani305@gmail.com>
2018-03-13 16:50:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,44 +13,37 @@ WIN_COMBINATIONS = [
   [0,4,8], #left_diagonal_win
   [2,4,6] #right_diagonal_win
 ]
-def won? (board)
 
-  WIN_COMBINATIONS.detect do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-    if board[win_index_1] == board[win_index_2] && board[win_index_2] == board[win_index_3] && board[win_index_1] != " "
-      return win_combination
-    else
-    false
-    end
+def won?(board)
+  WIN_COMBINATIONS.find do |win_combo|
+  # win_combo.find do |win_index|
+    board[win_combo[0]] == board[win_combo[1]] && board[win_combo[0]] == board[win_combo[2]] && position_taken?(board, win_combo[1])
+    ##   end #!position_taken?(board, index)
   end
 end
 
-def full? (board)
-
+def full?(board)
   board.all? do |full_board|
-      if full_board == "X" || full_board == "O"
-        full_board
-    end
-  end
+  full_board == "X" || full_board == "O" || full_board == "nil"
+end
 end
 
 def draw?(board)
-   !won?(board) && full?(board)
+  won?(board) == nil && full?(board) == true
 end
 
-def over? (board)
-  won?(board) || full?(board) || draw?(board)
+def over?(board)
+  won?(board) || full?(board)
+ #!draw?(board) == nil && full?(board) == false
+ #return true
 end
 
-def winner (board)
-  if  winner_board = won?(board)
-    board[winner_board.first]
-  end
+def winner(board)
+  if(draw?(board) || !full?(board)) && !won?(board)
+    return nil
+  elsif (board[won?(board)[0]] == "X")
+    return "X"
+  elsif (board[won?(board)[0]] == "O")
+    return "O"
+end
 end
